47e068895ea6f997ab4ae60b5aa9ce2a7fb31b5b
Nicolas Coen <ncoen97@gmail.com>
2019-04-06 17:46:36 -0300

    Primer commit
    HolaMundo

---
+++ superioratr/superioratr/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace superioratr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotonHolaMundo = new System.Windows.Forms.Button();
+            this.LabelHolaMundo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BotonHolaMundo
+            // 
+            this.BotonHolaMundo.Location = new System.Drawing.Point(316, 180);
+            this.BotonHolaMundo.Name = "BotonHolaMundo";
+            this.BotonHolaMundo.Size = new System.Drawing.Size(75, 23);
+            this.BotonHolaMundo.TabIndex = 0;
+            this.BotonHolaMundo.Text = "Hola Mundo";
+            this.BotonHolaMundo.UseVisualStyleBackColor = true;
+            this.BotonHolaMundo.Click += new System.EventHandler(this.BotonHolaMundo_Click);
+            // 
+            // LabelHolaMundo
+            // 
+            this.LabelHolaMundo.AutoSize = true;
+            this.LabelHolaMundo.Location = new System.Drawing.Point(335, 130);
+            this.LabelHolaMundo.Name = "LabelHolaMundo";
+            this.LabelHolaMundo.Size = new System.Drawing.Size(0, 13);
+            this.LabelHolaMundo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LabelHolaMundo);
+            this.Controls.Add(this.BotonHolaMundo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotonHolaMundo;
+        private System.Windows.Forms.Label LabelHolaMundo;
+    }
+}
+
---
+++ superioratr/superioratr/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace superioratr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LabelHolaMundo.Hide();
+        }
+
+        private void BotonHolaMundo_Click(object sender, EventArgs e)
+        {
+            LabelHolaMundo.Text = "Hola Mundo :D";
+            LabelHolaMundo.Show();
+        }
+    }
+}
